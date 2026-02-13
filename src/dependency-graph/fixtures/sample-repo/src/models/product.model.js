001c94059ccc669fc1bd53f4b80ac9c50cfd084a
Muhammad Aitazaz Ahsan <152264393+aitazazahsan01@users.noreply.github.com>
2026-02-13 15:25:00 +0500

    test(fixtures): add mock files for product testing

---
+++ src/dependency-graph/fixtures/sample-repo/src/models/product.model.js
@@ -0,0 +1,8 @@
+// WIP: Initial module design - product.model.js
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../db.js';
+
+export const Product = sequelize.define('Product', {
+  name: DataTypes.STRING,
+  price: DataTypes.FLOAT,
+});
