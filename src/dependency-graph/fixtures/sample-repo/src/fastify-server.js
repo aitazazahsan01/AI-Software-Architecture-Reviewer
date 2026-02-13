121198b9bd0c2845146235db76f104f83a237cb6
Muhammad Aitazaz Ahsan <152264393+aitazazahsan01@users.noreply.github.com>
2026-02-13 17:46:00 +0500

    test(fixtures): add mock files for fastify-server testing

---
+++ src/dependency-graph/fixtures/sample-repo/src/fastify-server.js
@@ -0,0 +1,14 @@
+// WIP: Initial module design - fastify-server.js
+import Fastify from 'fastify';
+
+const fastify = Fastify();
+
+fastify.route({
+  method: 'GET',
+  url: '/orders/:id',
+  handler: async (request, reply) => {
+    return { id: request.params.id };
+  },
+});
+
+export default fastify;
