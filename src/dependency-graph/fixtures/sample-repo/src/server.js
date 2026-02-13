4e4c0a709a95548fae6333f11d9bab01808a3d8f
Muhammad Aitazaz Ahsan <152264393+aitazazahsan01@users.noreply.github.com>
2026-02-13 16:42:00 +0500

    test(fixtures): add mock files for server testing

---
+++ src/dependency-graph/fixtures/sample-repo/src/server.js
@@ -0,0 +1,14 @@
+// WIP: Initial module design - server.js
+import express from 'express';
+
+const app = express();
+
+app.get('/users/:id', (req, res) => {
+  res.json({ id: req.params.id });
+});
+
+app.post('/users', (req, res) => {
+  res.status(201).json({ created: true });
+});
+
+export default app;
