37f4d64092add22e2a9400426912bce4246e4d6e
Muhammad Aitazaz Ahsan <152264393+aitazazahsan01@users.noreply.github.com>
2026-02-13 10:30:00 +0500

    feat: add initial skeleton structure for index

---
+++ src/dependency-graph/index.ts
@@ -0,0 +1,4 @@
+// WIP: Initial module design - index.ts
+export { buildDependencyGraph } from './graph.js';
+export { extractApiEndpoints } from './api-endpoints.js';
+export { extractDataModels } from './data-models.js';
