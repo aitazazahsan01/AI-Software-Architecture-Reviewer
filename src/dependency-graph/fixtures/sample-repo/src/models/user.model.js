736e617c38e3e098db09dfee03d149ab2f85a3b4
Muhammad Aitazaz Ahsan <152264393+aitazazahsan01@users.noreply.github.com>
2026-02-13 14:09:00 +0500

    test(fixtures): add mock files for user testing

---
+++ src/dependency-graph/fixtures/sample-repo/src/models/user.model.js
@@ -0,0 +1,11 @@
+// WIP: Initial module design - user.model.js
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: String,
+  email: { type: String, required: true },
+  age: Number,
+});
+
+export const User = mongoose.model('User', userSchema);
