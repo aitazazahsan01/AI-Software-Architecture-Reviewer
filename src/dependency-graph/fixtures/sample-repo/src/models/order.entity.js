9b8ae50b0ba46002809978766098ed9c09f4abf9
Muhammad Aitazaz Ahsan <152264393+aitazazahsan01@users.noreply.github.com>
2026-02-13 15:56:00 +0500

    test(fixtures): add mock files for order testing

---
+++ src/dependency-graph/fixtures/sample-repo/src/models/order.entity.js
@@ -0,0 +1,14 @@
+// WIP: Initial module design - order.entity.js
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Order {
+  @PrimaryGeneratedColumn()
+  id;
+
+  @Column()
+  status;
+
+  @Column()
+  total;
+}
