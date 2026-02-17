d1c4689932a975f3107acf6f4b71e875d2a40c07
Muhammad Aitazaz Ahsan <152264393+aitazazahsan01@users.noreply.github.com>
2026-02-17 11:01:00 +0500

    feat: add initial skeleton structure for index

---
+++ src/rag/index.ts
@@ -0,0 +1,4 @@
+// WIP: Initial module design - index.ts
+export { embedTexts, EMBEDDING_BATCH_SIZE, EMBEDDING_DIMENSIONS } from './embed.js';
+export type { EmbeddingClient } from './embed.js';
+export { VectorStore, cosineSimilarity } from './store.js';
